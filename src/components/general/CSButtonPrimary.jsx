ee2e35d685188904570e144e3d8c94a5bdf60bc3
Edwin van de Ven <edwin@simplycode.nl>
2023-06-02 21:51:09 +0200

    Forward refs properly in CSButtonPrimary component

--- src/components/general/CSButtonPrimary.jsx
+++ src/components/general/CSButtonPrimary.jsx
@@ -1,28 +1,32 @@
 // @ts-check
 
-import React from 'react'
-import { Link } from "react-router-dom"
+// https://react.dev/reference/react/forwardRef
+import React, { forwardRef} from 'react'
 import { Button } from "@chakra-ui/react"
 
-export default function CSButtonPrimary({href, buttonText, ...rest}) {  
+function CSButtonPrimary(props, ref) { 
+const { buttonText, ...rest } = props
+
   return (
-    <Link to={href}>
-      <Button
-        mt={10}
-        w={'full'}
-        bg={'green.400'}
-        color={'white'}
-        rounded={'xl'}
-        boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
-        _hover={{
-          bg: 'green.500',
-        }}
-        _focus={{
-          bg: 'green.500',
-        }}
-        {...rest}>
-        {buttonText}
-      </Button>
-    </Link> 
+    <Button
+      mt={10}
+      w={'full'}
+      bg={'green.400'}
+      color={'white'}
+      rounded={'md'}
+      boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
+      _hover={{
+        bg: 'green.500',
+      }}
+      _focus={{
+        bg: 'green.500',
+      }}
+      ref={ref}
+      {...rest}
+    >
+      {buttonText}
+    </Button>
   )
 }
+
+export default forwardRef(CSButtonPrimary)

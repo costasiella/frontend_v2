c5a5998421af504fe273413a33e97c67f797c311
Edwin van de Ven <edwin@simplycode.nl>
2023-06-08 21:39:42 +0200

    Add CSCardHeading component

---
+++ src/components/general/CSCardHeading.jsx
@@ -0,0 +1,14 @@
+// @ts-check
+
+import React from 'react'
+import { Heading } from "@chakra-ui/react"
+
+export default function CSCardheading(props) { 
+  const { children, ...rest } = props
+
+  return (
+    <Heading as="h3" size="sm" mb={6} {...rest}>
+      {children}
+    </Heading>
+  )
+}
